e7b72b2e467f4d0f9d84ceac24d66cbcbd874c58
saman008 <1975566197@qq.com>
2026-02-10 14:30:33 +0800

    0.0.1

---
+++ SMSDK.podspec
@@ -0,0 +1,48 @@
+Pod::Spec.new do |spec|
+
+
+  spec.name         = "SMSDK"
+  spec.version      = "0.0.1"
+  spec.summary      = "简短的SDK描述"
+
+  spec.description  = <<-DESC
+  这是对SDK的一段描述
+                   DESC
+
+  spec.homepage     = "https://github.com/saman008/SMSDK"
+
+  spec.license      = "MIT (example)"
+  spec.license      = { :type => "MIT", :file => "FILE_LICENSE" }
+
+  spec.author       = { "saman008" => "1975566197@qq.com" }
+
+  spec.platform     = :ios, "15.0"
+
+  spec.source       = { :git => "https://github.com/saman008/SMSDK.git", :tag => "#{spec.version}" }
+
+
+  spec.vendored_frameworks  = "SMSDK.xcframework"
+
+  spec.source_files = 'SMSDK/*.{swift,h,m}'
+  spec.resource_bundles = {
+    'SMSDK' => ['SMSDK.xcframework/ios-arm64/SMSDK.framework/SMSDK.bundle/**/*.{storyboard,xib,xcassets,json,imageset,png,strings,mp3}'],
+    'SMSDK' => ['SMSDK.xcframework/ios-arm64_x86_64-simulator/SMSDK.framework/SMSDK.bundle/**/*.{storyboard,xib,xcassets,json,imageset,png,strings,mp3}'],
+  }
+
+
+  # 必须的框架
+   spec.frameworks = 'UIKit', 'Foundation'
+    # 静态框架
+   spec.static_framework = true
+   spec.ios.deployment_target = '15.0'
+   spec.swift_version = ['5.0']
+  #以下是你引用的第三方库 最好把三方库对应的版本加上
+   spec.dependency "RxCocoa", "~> 6.6.0"
+   spec.dependency "SnapKit"
+   spec.dependency "RxSwift"
+   spec.dependency "RxDataSources"
+   spec.dependency "RxGesture"
+   spec.dependency "Differentiator"
+   spec.dependency "RxRelay"
+
+end
---
+++ SMSDK.xcframework/ios-arm64_x86_64-simulator/SMSDK.framework/Headers/SMSDK.swift
@@ -0,0 +1,48 @@
+//
+//  SMSDK.swift
+//  SMSDK
+//
+//  Created by youliaobf on 2026/2/9.
+//
+
+import UIKit
+
+
+open class SMSDK: NSObject {
+    
+    public static func getBundleImage(target: UIViewController) {
+        let img = UIImage.bundledImage(named: "icon-20")
+        let img1 = UIImage.bundledImage(named: "icon-20.png")
+        let img2 = UIImage.bundledImage(named: "icon-30")
+        let img3 = UIImage.bundledImage(named: "icon-30.png")
+        let img4 = UIImage.bundledImage(named: "icon-40")
+        let img5 = UIImage.bundledImage(named: "icon-40.png")
+        print(img)
+        print(img1)
+        print(img2)
+        print(img3)
+        print(img4)
+        print(img5)
+        if let image = UIImage.bundledImage(fileName: "icon-40.png"){
+            print("✅ \(image) 加载成功: \(image.size)")
+        } else {
+            print("❌ 加载失败")
+        }
+        if let image = UIImage.bundledImage(fileName: "icon-20.png"){
+            print("✅ \(image) 加载成功: \(image.size)")
+        } else {
+            print("❌ 加载失败")
+        }
+        let imageview1 = UIImageView(image: img)
+        imageview1.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        let imageview2 = UIImageView(image: img2)
+        imageview2.frame = CGRect(x: 100, y: 200, width: 100, height: 100)
+        
+        let imageview3 = UIImageView(image: img4)
+        imageview1.frame = CGRect(x: 100, y: 300, width: 100, height: 100)
+        
+        target.view.addSubview(imageview1)
+        target.view.addSubview(imageview2)
+        target.view.addSubview(imageview3)
+    }
+}
